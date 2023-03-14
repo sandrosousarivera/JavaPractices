d577ba0d173248d2b23ef9a3800f11c0bd63453e
Sandro Sousa <105113293+sandrosousarivera@users.noreply.github.com>
2023-03-14 12:19:12 +0100

    Guess the random number
    Simple game where I practice the use of loops, the game will give you a random number and give you hints in order for you to guess it.

---
+++ GuessTheRandomNumber.java
@@ -0,0 +1,47 @@
+package Default;
+
+import java.util.*;
+
+public class GuessTheRandomNumber {
+
+	public static void main(String[] args) {
+
+		int randomNumber = (int)(Math.random()*100);
+		
+		Scanner input = new Scanner(System.in);
+		
+		int userNumber=0;
+		
+		int tries=0;
+		
+		System.out.println("I'm gonna guess a number between 0 and 100, can you guess what number it is?");
+		
+		while(userNumber != randomNumber) {
+			
+			tries ++;
+			
+			System.out.println("Enter a number, please.");
+			
+			userNumber = input.nextInt(); 
+			
+			if(randomNumber < userNumber) {
+				
+				System.out.println("The number I thought of is LOWER than that.");
+				
+			}
+			
+			else if (randomNumber > userNumber) {
+				
+				System.out.println("The number I thought of is HIGHER than that.");
+				
+			}
+			
+		}
+		
+		
+		System.out.println("HOORAY! you guessed the number in " + tries + " tries");
+		
+		
+	}
+
+}
